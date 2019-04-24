1feb3eaa5b6e52bad205c45daefca7fc3b5ec359
sirfossilot <45555140+sirfossilot@users.noreply.github.com>
2019-04-23 23:13:02 -0700

    initial commit

---
+++ homework06/Collatz.java
@@ -0,0 +1,30 @@
+/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * File name  :  Collatz.java
+ * Purpose    :  Running Collatz sequence and returning the number of steps it takes.
+ * @author    :  Fosse Lin-Bianco
+ * Date       :  2019-04-04
+ * Description:  @see <a href='http://bjohnson.lmu.build/cmsi186web/homework06.html'>Assignment Page</a>
+ * Notes      :  None
+ * Warnings   :  None
+ *
+ *  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * Revision History
+ * ================
+ *   Ver      Date     Modified by:      Reason for change or modification
+ *  -----  ----------  ------------      ---------------------------------------------------------------------
+ *  1.0.0  2019-04-10  Fosse Lin-Bianco  Initial writing and begin coding
+ *
+ *
+ *  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+public class BrobInt {
+
+
+  /** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+   *  the main method redirects the user to the test class
+   *  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+   public static void main( String[] args ) {
+      System.out.println( "\n  Hello, world, from the Collatz program!!\n" );
+
+   }
+}
