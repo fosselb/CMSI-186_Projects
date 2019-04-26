496044c56ded4133dc11e0ee7c1df505fcdcd6a4
sirfossilot <45555140+sirfossilot@users.noreply.github.com>
2019-04-25 23:11:28 -0700

    Added algorithm. Have not tested it.

--- homework06/Collatz.java
+++ homework06/Collatz.java
@@ -18,13 +18,33 @@
  *  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
 public class Collatz {
+  public BrobInt arg = null;
+  int count = 0;
 
+  /** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+   *  Method to run the Collatz Sequence
+   *  @param  bint         BrobInt to be placed into Collatz sequence
+   *  @return BrobInt that is the number of steps needed to complete the sequence
+   *  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+   public BrobInt runCollatz( BrobInt bint ) {
+     BrobInt arg = new BrobInt(bint.internalValue);
+     arg.sign = bint.sign;
+     while (arg.numsReversed[0] % 2 > 1) {
+       arg = (arg.numsReversed[0] % 2 == 0) ? arg.divide(BrobInt.TW0) : (arg.multiply(BrobInt.THREE).add(BrobInt.ONE));
+       count++;
+     }
+     return new BrobInt(Integer.toString(count));
+     //throw new UnsupportedOperationException( "\n         Sorry, that operation is not yet implemented." );
+   }
 
   /** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    *  the main method redirects the user to the test class
    *  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
    public static void main( String[] args ) {
-      System.out.println( "\n  Hello, world, from the Collatz program!!\n" );
+     System.out.println( "\n  Hello, world, from the Collatz program!!\n" );
+     arg = new BrobInt(args[0]);
+     arg.runCollatz(arg);
+     }
+
 
    }
-}
