b5e936908d86d679c27d361ac88ac83c52680612
Fosse Lin-Bianco <fosselin-bianco@TEDs-iMac.local>
2019-05-14 23:32:33 -0700

    Add to throwDart() method.

--- homework08/PiSolver.java
+++ homework08/PiSolver.java
@@ -17,6 +17,7 @@
  *  1.0.2  2019-05-14  Fosse Lin-Bianco  Change version numbers.
  *  1.0.3  2019-05-14  Fosse Lin-Bianco  Add estimate pi method.
  *  1.0.4  2019-05-14  Fosse Lin-Bianco  Formatting changes.
+ *  1.0.5  2019-05-14  Fosse Lin-Bianco  Add to throwDart() method.
  *  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
 public class PiSolver {
@@ -25,6 +26,7 @@ public class PiSolver {
    */
    public static double dartsInCircle;
    public static double totalDarts;
+   public static int numberOfDarts = 0;
 
    /**
     * Method that generates two random number between -1 and 1
@@ -41,7 +43,9 @@ public class PiSolver {
     * Method that simulates throwing darts at a circle, radius 1, wihin a sqaure
     */
    public static void throwDart() {
-
+     double x = generateRandomValues();
+     double y = generateRandomValues();
+     Dart d = new Dart(x, y);
    }
 
    /**
@@ -56,6 +60,16 @@ public class PiSolver {
   public static void main(String[] args) {
     PiSolver p = new PiSolver();
 
+    //simulating throwing darts
+    // while (numberOfDarts < 1000) {
+    //   p.throwDart();
+    // }
+
+    System.out.println("Testing: ");
+    Dart d1 = new Dart(0.2, 0.2);
+    System.out.println("X: " + d1.positionX);
+    System.out.println("Y: " + d1.positionY);
+    System.out.println("Distance: " + d1.distanceFromOrigin);
   }
 
 }
